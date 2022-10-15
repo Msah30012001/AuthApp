29bec9610d9af1e2414ea46db5b6d8f22f2fa6a0
Manish Shambhoo Sah <msah74915@gmail.com>
2022-10-15 15:14:59 +0530

    docs: gate implementation in laravel to manage multiple role

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,13 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function private(){
+        if(Gate::allows('admin-only',auth()->user())){
+            return view('private');
+        }else{
+            abort(403);
+        }
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::define('admin-only',function($user){
+            if($user->isAdmin){
+                return true;
+            }
+            return false;
+        });
         //
     }
 }
